1135f4f6e6cca2905c9c3f3c6eae310c1f5332d2
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-17 12:15:18 +0200

    Add error message for negative numbers and carry out remaining tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,14 @@
 const repeatString = function(string, num) {
     let result = "";
+    if(num < 0) {
+        result = "ERROR";
+    }
     for (let i = 0; i < num; i++) {
         result += string
     }
     
     return result
 };
-
-// TODO: FIND A WAY TO RUN THE CODE SO AS TO TEST WHAT HAPPENS WHEN YOU USE A NEGATIVE NUMBER
 
 // Do not edit below this line
 module.exports = repeatString;
