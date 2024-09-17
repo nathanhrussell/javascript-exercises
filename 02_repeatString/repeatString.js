aaad894fd2850a0020d03f08cdab8471f3fcab3d
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-17 09:55:38 +0200

    Add repeatString function content

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    for (let i = 0, i < num, i++) {
+        result += string
+    }
+    
+    return string
 };
 
 // Do not edit below this line
