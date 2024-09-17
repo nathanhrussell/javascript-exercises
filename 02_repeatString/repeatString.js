5aceb03f651e470bf98f8f804a9c92d4621fc233
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-17 10:17:25 +0200

    Remove skip for negative num test and add todo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -7,5 +7,7 @@ const repeatString = function(string, num) {
     return result
 };
 
+// TODO: FIND A WAY TO RUN THE CODE SO AS TO TEST WHAT HAPPENS WHEN YOU USE A NEGATIVE NUMBER
+
 // Do not edit below this line
 module.exports = repeatString;
