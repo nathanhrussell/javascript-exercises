7534e39033fce343834d77b9d87d0a4f9f886a4a
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-17 18:51:51 +0200

    Add removeFromArray logic to past first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ele) {
+    let indexToRemove = arr.indexOf(ele);
+    let numberToRemove = String(ele).length;
+
+    console.log(indexToRemove);
+    console.log(numberToRemove);
+
+    arr.splice(indexToRemove, numberToRemove);
+    
+    return arr;
 };
 
 // Do not edit below this line
