d4ff348b4815b51f4efd50777df420e71efd19d4
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-19 19:35:21 +0200

    Add elipsis to elements argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,11 @@
-const removeFromArray = function(arr, elements) {
+const removeFromArray = function (array, ...elements) {
     
-}
 
+
+  };
+  
+
+console.log(removeFromArray([1, 2, 2, 3], 2));
 
 
 // Do not edit below this line
