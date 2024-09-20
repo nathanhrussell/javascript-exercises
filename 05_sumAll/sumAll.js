3af516885cb6ab25dc611653ef21aea23a829578
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-20 12:36:53 +0200

    Add sumAll function logic to pass first two tests and add todo

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let numArray = [];
+    sum = 0;
+    while (num1 <= num2){
+        numArray.push(num1++);
+    }
+
+    for (let i = 0; i <numArray.length; i++) {
+        sum += numArray[i];
+    }
+
+    return sum;
+
+    
 };
+
+
+// TEST WITH FOLLOWING CASE: expect(sumAll(123, 1)).toEqual(7626);
+// IF NUM 1 BIGGER THAN NUM 2 THEN REVERSE VALUES?
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
