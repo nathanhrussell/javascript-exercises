9efbf75a98decaf2c5a39ad821ece250c9556a79
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-23 10:55:18 +0200

    Refactor if statement for error checking

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,16 @@
 const sumAll = function(num1, num2) {
 
-    if(num1 < 0 || num2 < 0) {
-        return "ERROR";
-    }
-
-    if(num1 % 1 != 0 || num2 % 1 != 0) {
-        return "ERROR";
+    if(num1 < 0 || 
+       num2 < 0 ||
+       num1 % 1 != 0 ||
+       num2 % 1 != 0 ||
+       typeof(num1) != "number" || 
+       typeof(num2) != "number") {
+       return "ERROR";
     }
 
     if(num1 > num2) {
         [num1, num2] = [num2, num1];
-    }
-
-    if(typeof(num1) != "number" || typeof(num2) != "number") {
-        return "ERROR";
     }
 
     let numArray = [];
