de2d229d7e09959864b72fc115bd791b820e7b56
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-23 10:49:08 +0200

    Add logic to switch variables if first arg larger and test for floats and negative nums

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,20 @@
 const sumAll = function(num1, num2) {
 
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if(num1 % 1 != 0 || num2 % 1 != 0) {
+        return "ERROR";
+    }
+
+    if(num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+
     let numArray = [];
     sum = 0;
+    
     while (num1 <= num2){
         numArray.push(num1++);
     }
@@ -11,15 +24,13 @@ const sumAll = function(num1, num2) {
     }
 
     return sum;
-
-    
 };
 
-
-// TEST WITH FOLLOWING CASE: expect(sumAll(123, 1)).toEqual(7626);
-// IF NUM 1 BIGGER THAN NUM 2 THEN REVERSE VALUES?
-
-console.log(sumAll(1, 4));
+let num1 = 2;
+let num2 = 4;
+if(num1 % 1 != 0 || num2 % 1 != 0) {
+    console.log("ERROR");
+}
 
 // Do not edit below this line
 module.exports = sumAll;
