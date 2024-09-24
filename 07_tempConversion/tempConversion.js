4e1a900c0197582f7f18b8b223107d9c1f85db19
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-24 12:14:34 +0200

    Add logic to convertToFahrenheit function to pass three tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -6,12 +6,15 @@ const convertToCelsius = function(temp) {
 
 
 const convertToFahrenheit = function(temp) {
+  let fahrenheitConversion = (temp * (9/5) + 32);
+  let roundedFahrenheit = Math.round(fahrenheitConversion * 10) / 10;
+  return roundedFahrenheit;
 };
 
-let temp = 100;
-let celsiusConversion = ((temp - 32) * (5/9));
-let roundedCelsius = Math.round(celsiusConversion * 10) / 10;
-console.log(roundedCelsius);
+let temp = 14;
+let fahrenheitConversion = (temp * (9/5) + 32);
+let roundedFahrenheit = Math.round(fahrenheitConversion * 10) / 10;
+console.log(roundedFahrenheit);
 
 // Do not edit below this line
 module.exports = {
