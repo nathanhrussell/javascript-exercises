6503a400d9d5d0ca436c5267f6aea540f953a793
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-10-24 11:54:44 +0200

    Add logic to pass all fibonacci test cases

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if(typeof(n) === "string") {
+        n = Number(n)
+    }
+
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    else if (n <= 1) {
+        return n;
+    }
+
+
+    return fibonacci(n - 1) + fibonacci(n-2);
 };
+
+let n = "1";
+
+console.log(n);
 
 // Do not edit below this line
 module.exports = fibonacci;
