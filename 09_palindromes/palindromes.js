5e338316ee5883a11a2338db816847a0544e9b57
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-10-24 10:46:28 +0200

    Add function code to solve entire palindrome test suite

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strStripped = str.replace(/[\p{P}\s$+<=>^`|~]/gu, "").toLowerCase();
+    let strReversed = strStripped.split("").reverse().join("");
+    return (strStripped === strReversed) ? true : false;
 };
 
 // Do not edit below this line
