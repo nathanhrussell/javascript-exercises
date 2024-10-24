44c7d511a392a731fbd693e048d5964b2365a107
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-10-24 10:15:27 +0200

    Add all aspects to solve calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, n) => product * n, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let f = [];
+  if (num == 0 || num == 1)
+    return 1;
+  if (f[num] > 0)
+    return f[num];
+  return f[num] = factorial(num-1) * num;
 };
 
 // Do not edit below this line
