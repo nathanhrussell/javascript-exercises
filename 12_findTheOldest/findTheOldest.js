0cee6287feb98591e9185fa12eb0511dc0f7b938
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-10-28 19:38:42 +0100

    Add function logic to pass first two tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+  return people.reduce((acc, person) => {
+  const yearsLived = person.yearOfDeath - person.yearOfBirth;
+    return yearsLived > acc.years
+    ? { name: person.name, years: yearsLived }
+    : acc;
+}, {name: '', years: 0 });
 };
 
 // Do not edit below this line
