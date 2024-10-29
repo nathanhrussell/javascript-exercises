6237e25e3874e68972f28331b12480368f4acd83
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-10-29 17:27:28 +0100

    Add logic to solve all three test cases

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,11 @@
 const findTheOldest = (people) => {
   return people.reduce((acc, person) => {
-  const yearsLived = person.yearOfDeath - person.yearOfBirth;
+    const currentYear = new Date().getFullYear();
+    const yearsLived = (person.yearOfDeath || currentYear) - person.yearOfBirth;
     return yearsLived > acc.years
-    ? { name: person.name, years: yearsLived }
-    : acc;
-}, {name: '', years: 0 });
+      ? { name: person.name, years: yearsLived }
+      : acc;
+  }, { name: '', years: 0 });
 };
 
 // Do not edit below this line
